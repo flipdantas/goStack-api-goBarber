aeedc2eab443ab65aafcde36185a065dd94075cd
Filipe Dantas <filipedantas90@hotmail.com>
2021-03-01 16:47:20 -0300

    Arq. e DDD

---
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticateUser = container.resolve(AuthenticateUserService);
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    // delete user.password não pode mais ser usado, usar:
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ user: userWithoutPassword });
+  }
+}
